caceda1fe83c4bf0978242ab78d9f3cd033a3338
TheFirstHokage <kendrickutwhite@gmail.com>
2022-06-06 19:45:54 -0700

    initial commit

---
+++ src/main/java/knight/springframework/spring5webapplication2/Spring5webapplication2Application.java
@@ -0,0 +1,21 @@
+package knight.springframework.spring5webapplication2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//GITHUB NOTES
+//1. git init -b main
+//2. git add .
+//3. git commit -m "initial commit"
+//4. git remote add origin  <REMOTE_URL>
+//5. git push origin main
+
+//NOTES
+//TURN JAVA POJO into JPA entities
+@SpringBootApplication
+public class Spring5webapplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webapplication2Application.class, args);
+	}
+
+}
---
+++ src/test/java/knight/springframework/spring5webapplication2/Spring5webapplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package knight.springframework.spring5webapplication2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webapplication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
